666062583fbf3598d6aab234e15dbd5ee42a2e40
MatoiNRyuuko <476411907MatoiRyuuko@users.noreply.github.com>
2018-03-20 08:11:16 +0800

    Lintcode：丑数
    思考题

---
+++ Lintcode练习代码/UglyNum.java
@@ -0,0 +1,26 @@
+package killLint;
+
+public class UglyNum {
+	 public boolean isUgly(int num) {
+	        // write your code here
+	        if(num == 0){
+	            return false;
+	        }
+	        while(num%2 == 0){
+	            num = num/2;
+	        }
+	        while(num%3 == 0){
+	            num = num/3;
+	        }
+	        while(num%5 == 0){
+	            num = num /5;
+	        }
+	        
+	        if(num == 1){
+	            return true;
+	        }
+	        else{
+	            return false;
+	        }
+	    }
+}
