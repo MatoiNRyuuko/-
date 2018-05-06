67b2becf1089cd1416f4efb14b44c24a9786936e
MatoiNRyuuko <476411907MatoiRyuuko@users.noreply.github.com>
2018-05-06 22:44:02 +0800

    lintcode：经典二分查找
    重新回归做题的日子，做几道简单题醒醒脑。

---
+++ Lintcode练习代码/FindPosition.java
@@ -0,0 +1,25 @@
+package killLint;
+
+public class FindPosition {
+	public int findPosition(int[] nums, int target) {
+        // write your code here
+        if(nums.length == 0){
+            return -1;
+        }
+        int high = nums.length-1,low = 0;
+        int mid = -1;
+        while(low != high){
+            mid = (high+low)/2;
+            if(nums[mid] > target){
+                high = mid -1;
+            }else if(nums[mid] < target){
+                low = mid +1;
+            }else{
+                return mid;
+            }
+            if(nums[low] > target || nums[high] < target){
+                return -1;
+            }
+        return -1;
+    }
+}
