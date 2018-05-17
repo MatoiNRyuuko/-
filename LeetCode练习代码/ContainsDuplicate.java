133f06dd4a2d44a5bb03840b93b907c898631e73
MatoiNRyuuko <476411907MatoiRyuuko@users.noreply.github.com>
2018-05-17 14:11:54 +0800

    leetcode：存在重复
    学到了Array.sort真是神奇的方法。

---
+++ LeetCode练习代码/ContainsDuplicate.java
@@ -0,0 +1,18 @@
+package killLeet;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+	public boolean containsDuplicate(int[] nums) {
+        if (nums.length <= 1) {
+            return false;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
